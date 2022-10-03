b7bb0269ee3d0816437b65e38b5ee688beb013b4
SOUFIANEXIV <98940737+SOUFIANEXIV@users.noreply.github.com>
2022-10-03 21:50:49 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
-import {useEffect} from 'react'
+/*import {useEffect} from 'react' */
 import Layout from './components/Layout';
 
 
